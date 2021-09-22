daed36732a582e67fdc4fb2bfd42a370e736093f
Jason Belgard <belgard.jason@gmail.com>
2021-09-22 09:01:14 -0500

    Variables exercise complete

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1
+// const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+num += 7;
+
+num -= 1;
+
+num = "THis is not a number";
+
+let blank;
+
+console.log(blank);
+
+blank = null;
+
+// bonus
+// 1
+
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1]);
